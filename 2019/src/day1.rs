fe60c0ddb7f72389af827ce1f959ae4e391cca66
Alexey Sokolov <sokolov@google.com>
2019-12-01 10:08:09 +0000

    Make cargo clippy happy

--- 2019/src/day1.rs
+++ 2019/src/day1.rs
@@ -5,12 +5,12 @@ fn parse(input: &str) -> Result<Vec<i32>, std::num::ParseIntError> {
 	input.lines().map(|l| l.parse()).collect()
 }
 
-fn fuel_for(mass: &i32) -> i32 {
+fn fuel_for(mass: i32) -> i32 {
 	mass / 3 - 2
 }
 
-fn total_fuel_for(mass: &i32) -> i32 {
-	std::iter::successors(Some(*mass), |m| {
+fn total_fuel_for(mass: i32) -> i32 {
+	std::iter::successors(Some(mass), |&m| {
 		let new = fuel_for(m);
 		if new < 0 {
 			None
@@ -24,12 +24,12 @@ fn total_fuel_for(mass: &i32) -> i32 {
 
 #[aoc(day1, part1)]
 fn part1(masses: &[i32]) -> i32 {
-	masses.iter().map(fuel_for).sum()
+	masses.iter().cloned().map(fuel_for).sum()
 }
 
 #[aoc(day1, part2)]
 fn part2(masses: &[i32]) -> i32 {
-	masses.iter().map(total_fuel_for).sum()
+	masses.iter().cloned().map(total_fuel_for).sum()
 }
 
 #[cfg(test)]
@@ -38,17 +38,17 @@ mod tests {
 
 	#[test]
 	fn part1_fuel() {
-		assert_eq!(fuel_for(&12), 2);
-		assert_eq!(fuel_for(&14), 2);
-		assert_eq!(fuel_for(&1969), 654);
-		assert_eq!(fuel_for(&100756), 33583);
+		assert_eq!(fuel_for(12), 2);
+		assert_eq!(fuel_for(14), 2);
+		assert_eq!(fuel_for(1969), 654);
+		assert_eq!(fuel_for(100756), 33583);
 	}
 
 	#[test]
 	fn part2_fuel() {
-		assert_eq!(total_fuel_for(&12), 2);
-		assert_eq!(total_fuel_for(&14), 2);
-		assert_eq!(total_fuel_for(&1969), 966);
-		assert_eq!(total_fuel_for(&100756), 50346);
+		assert_eq!(total_fuel_for(12), 2);
+		assert_eq!(total_fuel_for(14), 2);
+		assert_eq!(total_fuel_for(1969), 966);
+		assert_eq!(total_fuel_for(100756), 50346);
 	}
 }
