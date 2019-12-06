ba92a29343231384f19c142460c102f543cf4f8d
Alexey Sokolov <sokolov@google.com>
2019-12-06 13:33:27 +0000

    Don't use recursion on huge inputs

--- 2019/src/day6.rs
+++ 2019/src/day6.rs
@@ -8,8 +8,11 @@ struct Day6Parser;
 #[derive(Debug)]
 struct Input(std::collections::HashMap<i32, i32>);
 
-fn parse_num(input: &str) -> Result<i32, std::num::ParseIntError> {
-	i32::from_str_radix(input, 36)
+fn parse_num(input: &str) -> i32 {
+	let a = input.chars().nth(0).unwrap_or_default() as i32;
+	let b = input.chars().nth(1).unwrap_or_default() as i32;
+	let c = input.chars().nth(2).unwrap_or_default() as i32;
+	(a * 256 + b) * 256 + c
 }
 
 #[aoc_generator(day6)]
@@ -20,46 +23,48 @@ fn parse(input: &str) -> Result<Input, crate::Error> {
 		.filter(|pair| pair.as_rule() == Rule::orbit)
 		.map(|pair| -> Result<(i32, i32), crate::Error> {
 			let mut orb = pair.into_inner();
-			let center = parse_num(orb.next()?.as_str())?;
-			let object = parse_num(orb.next()?.as_str())?;
+			let center = parse_num(orb.next()?.as_str());
+			let object = parse_num(orb.next()?.as_str());
 			Ok((object, center))
 		})
 		.collect();
 	Ok(Input(orbits?))
 }
 
-fn distance(input: &Input, what: i32, cache: &mut std::collections::HashMap<i32, i32>) -> i32 {
-	if let Some(v) = cache.get(&what) {
-		return *v;
-	}
-	let result = if what == 16438
-	/* COM */
-	{
-		0
-	} else if let Some(parent) = input.0.get(&what) {
-		let result = distance(input, *parent, cache) + 1;
-		result
-	} else {
-		0
-	};
-	cache.insert(what, result);
-	result
-}
-
 #[aoc(day6, part1)]
-fn part1(input: &Input) -> i32 {
+fn part1(input: &Input) -> usize {
 	let mut cache = std::collections::HashMap::new();
+	cache.insert(/* COM */ 4411213, 0);
 	input
 		.0
 		.keys()
-		.map(|&what| distance(input, what, &mut cache))
+		.map(|&what| -> usize {
+			let mut x = what;
+			let mut stack = Vec::new();
+			let dist;
+			loop {
+				if let Some(y) = cache.get(&x) {
+					dist = *y;
+					break;
+				}
+				stack.push(x);
+				if let Some(parent) = input.0.get(&x) {
+					x = *parent;
+				}
+			}
+			let len = stack.len();
+			for (d, z) in stack.into_iter().rev().enumerate() {
+				cache.insert(z, dist + d + 1);
+			}
+			dist + len
+		})
 		.sum()
 }
 
 #[aoc(day6, part2)]
 fn part2(input: &Input) -> Option<usize> {
-	let you = 44958;
-	let san = 36671;
+	let you = 5853013;
+	let san = 5456206;
 	let mut edges = std::collections::HashMap::<i32, Vec<i32>>::new();
 	for (&x, &y) in &input.0 {
 		edges.entry(x).or_default().push(y);
