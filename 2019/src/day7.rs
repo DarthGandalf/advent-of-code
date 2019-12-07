eef1968b64eaa1252d07fef88f5c3407e837e5ec
Alexey Sokolov <sokolov@google.com>
2019-12-07 08:12:02 +0000

    Day 7 part 1

---
+++ 2019/src/day7.rs
@@ -0,0 +1,51 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+use crate::intcode::run_copy;
+
+#[aoc_generator(day7)]
+fn parse(input: &str) -> Result<Vec<i32>, std::num::ParseIntError> {
+	input.split(',').map(|l| l.parse()).collect()
+}
+
+#[aoc(day7, part1)]
+fn part1(program: &[i32]) -> Result<i32, crate::Error> {
+	let results: Result<Vec<i32>, crate::Error> = permute::permutations_of(&[0, 1, 2, 3, 4])
+		.map(|x| -> Result<i32, crate::Error> {
+			let mut signal = vec![0];
+			for &amp in x {
+				signal.insert(0, amp);
+				let new_signal = run_copy(program, &signal, None)?.0;
+				signal = new_signal;
+			}
+			Ok(signal[0])
+		})
+		.collect();
+	Ok(results?.into_iter().max()?)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_part1() {
+		assert_eq!(
+			part1(&[3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]),
+			Ok(43210)
+		);
+		assert_eq!(
+			part1(&[
+				3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4,
+				23, 99, 0, 0
+			]),
+			Ok(54321)
+		);
+		assert_eq!(
+			part1(&[
+				3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33,
+				1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0
+			]),
+			Ok(65210)
+		);
+	}
+}
--- 2019/src/lib.rs
+++ 2019/src/lib.rs
@@ -72,5 +72,6 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 aoc_runner_derive::aoc_lib! { year = 2019 }
