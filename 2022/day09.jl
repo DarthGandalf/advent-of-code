6c0c107fa995f7ace7caa87987a149ccc909eb92
Alexey Sokolov <sokolov@google.com>
2022-12-09 08:54:55 +0000

    day 9 part 2

--- 2022/day09.jl
+++ 2022/day09.jl
@@ -15,74 +15,95 @@ D 1
 L 5
 R 2"""
 
-function part1(input)
-    was = Set([(0, 0)])
-    tx = 0
-    ty = 0
-    hx = 0
-    hy = 0
-    function adjust_tail()
-        if abs(hx - tx) <= 1 && abs(hy - ty) <= 1
-            return
-        elseif hx == tx || hy == ty
-            if hx == tx
-                if hy > ty + 1
-                    ty += 1
-                elseif hy < ty - 1
-                    ty -= 1
-                else
-                    error("assert 1")
-                end
+mutable struct Knot
+    x::Int
+    y::Int
+end
+function adjust_tail(h, t)
+    if abs(h.x - t.x) <= 1 && abs(h.y - t.y) <= 1
+        return
+    elseif h.x == t.x || h.y == t.y
+        if h.x == t.x
+            if h.y > t.y + 1
+                t.y += 1
+            elseif h.y < t.y - 1
+                t.y -= 1
             else
-                if hx > tx + 1
-                    tx += 1
-                elseif hx < tx - 1
-                    tx -= 1
-                else
-                    error("assert 2")
-                end
+                error("assert 1")
             end
         else
-            # move diagonally
-            if hx > tx
-                tx += 1
-            end
-            if hx < tx
-                tx -= 1
-            end
-            if hy > ty
-                ty += 1
-            end
-            if hy < ty
-                ty -= 1
+            if h.x > t.x + 1
+                t.x += 1
+            elseif h.x < t.x - 1
+                t.x -= 1
+            else
+                error("assert 2")
             end
         end
-        if abs(hx - tx) > 1 || abs(hy - ty) > 1
-            error("assert 3")
+    else
+        # move diagonally
+        if h.x > t.x
+            t.x += 1
         end
-        push!(was, (tx, ty))
+        if h.x < t.x
+            t.x -= 1
+        end
+        if h.y > t.y
+            t.y += 1
+        end
+        if h.y < t.y
+            t.y -= 1
+        end
     end
+    if abs(h.x - t.x) > 1 || abs(h.y - t.y) > 1
+        error("assert 3")
+    end
+end
+
+function part(input, L)
+    k = [Knot(0, 0) for i=1:L]
+    was = Set([(0, 0)])
     for line in eachsplit(input, '\n')
         dir, amounts = split(line, ' ')
         amount = parse(Int, amounts)
         for i=1:amount
+            h = k[1]
             if dir == "R"
-                hx += 1
+                h.x += 1
             elseif dir == "L"
-                hx -= 1
+                h.x -= 1
             elseif dir == "D"
-                hy += 1
+                h.y += 1
             elseif dir == "U"
-                hy -= 1
+                h.y -= 1
             else
                 error("wrong dir " * dir)
             end
-            adjust_tail()
+            for j=2:L
+                adjust_tail(k[j-1], k[j])
+            end
+            push!(was, (k[end].x, k[end].y))
         end
     end
     length(was)
 end
 
+part1(input) = part(input, 2)
+part2(input) = part(input, 10)
+
 solve(part1, real_input) do f
     @test f(test_input) == 13
+end
+
+solve(part2, real_input) do f
+    @test f(test_input) == 1
+    @test f("""
+            R 5
+            U 8
+            L 8
+            D 3
+            R 17
+            D 10
+            L 25
+            U 20""") == 36
 end
