511df1f4d6fd076ea9c3c8520d6781322a2de009
Alexey Sokolov <alexey@asokolov.org>
2024-11-30 16:43:08 +0000

    rust fmt

--- 2024/src/day01.rs
+++ 2024/src/day01.rs
@@ -1,12 +1,12 @@
-use aoc_runner_derive::{aoc, aoc_generator};
 use anyhow::Result;
+use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
 fn parse(input: &str) -> Result<String> {
-    Ok(input.to_string())
+	Ok(input.to_string())
 }
 
 #[aoc(day1, part1)]
 fn part1(input: &str) -> i32 {
-    0
+	0
 }
