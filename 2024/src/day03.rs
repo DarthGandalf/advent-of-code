148fddb83e19cd5f67b994d5aa18bf8ea314ad25
Alexey Sokolov <alexey@asokolov.org>
2024-12-03 23:07:59 +0000

    part 2

--- 2024/src/day03.rs
+++ 2024/src/day03.rs
@@ -1,20 +1,36 @@
+use anyhow::Result;
 use aoc_runner_derive::aoc;
 
 use nom::{
 	Err, IResult,
-	bytes::complete::{tag, take},
-	character::complete::{char, digit1},
+	branch::alt,
+	bytes::complete::tag,
+	character::complete::{anychar, char, digit1},
+	combinator::value,
 	error::{Error, ErrorKind},
+	multi::many0,
 	sequence::tuple,
 };
 
-fn mul(i: &str) -> IResult<&str, (u32, u32)> {
+#[derive(Debug, Clone)]
+enum Finding {
+	Mul(u32),
+	Do,
+	Dont,
+	Garbage,
+}
+
+fn mul(i: &str) -> IResult<&str, Finding> {
 	let (remainder, (_, a, _, b, _)) =
 		tuple((tag("mul("), digit1, char(','), digit1, char(')')))(i)?;
 	if a.len() > 3 || b.len() > 3 {
+		// ErrorKind::Tag is wrong, but what I'm supposed to return here?
 		return Err(Err::Error(Error::new(i, ErrorKind::Tag)));
 	}
-	Ok((remainder, (a.parse().unwrap(), b.parse().unwrap())))
+	Ok((
+		remainder,
+		Finding::Mul(a.parse::<u32>().unwrap() * b.parse::<u32>().unwrap()),
+	))
 }
 
 #[aoc(day3, part1)]
@@ -23,15 +39,41 @@ pub fn part1(input: &str) -> u32 {
 		.match_indices("mul")
 		.map(|(x, _)| {
 			let y = &input[x..];
-			let Ok((_, (a, b))) = mul(y) else { return 0 };
-			a * b
+			if let Ok((_, Finding::Mul(m))) = mul(y) {
+				m
+			} else {
+				0
+			}
 		})
 		.sum()
 }
 
 #[aoc(day3, part2)]
-pub fn part2(input: &str) -> usize {
-	0
+pub fn part2(input: &str) -> u32 {
+	let mut skip = false;
+	many0(alt((
+		mul,
+		value(Finding::Do, tag("do()")),
+		value(Finding::Dont, tag("don't()")),
+		value(Finding::Garbage, anychar),
+	)))(input)
+	.unwrap()
+	.1
+	.into_iter()
+	.map(|a| match a {
+		Finding::Mul(m) if skip => 0,
+		Finding::Mul(m) => m,
+		Finding::Do => {
+			skip = false;
+			0
+		}
+		Finding::Dont => {
+			skip = true;
+			0
+		}
+		Finding::Garbage => 0,
+	})
+	.sum()
 }
 
 #[cfg(test)]
