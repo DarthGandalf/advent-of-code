f6a3ac23f1f3058a69e0c6b35e2810980e6942f8
Alexey Sokolov <alexey@asokolov.org>
2024-12-24 01:19:51 +0000

    23

---
+++ 2024/src/day23.rs
@@ -0,0 +1,134 @@
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use aoc_runner_derive::aoc;
+use fnv::{FnvHashMap, FnvHashSet};
+use itertools::Itertools;
+
+fn parse(input: &str) -> (BTreeSet<&str>, BTreeMap<&str, BTreeSet<&str>>) {
+	let mut vertices = BTreeSet::<&str>::default();
+	let mut edges = BTreeMap::<&str, BTreeSet<&str>>::default();
+	for l in input.lines() {
+		let (a, b) = l.split('-').collect_tuple().unwrap();
+		edges.entry(a).or_default().insert(b);
+		edges.entry(b).or_default().insert(a);
+		vertices.insert(a);
+		vertices.insert(b);
+	}
+	(vertices, edges)
+}
+
+#[aoc(day23, part1)]
+pub fn part1(input: &str) -> usize {
+	let (vertices, edges) = parse(input);
+	let mut result = FnvHashSet::<String>::default();
+	for v in &vertices {
+		if v.chars().next().unwrap() != 't' {
+			continue;
+		}
+		for u in edges.get(v).unwrap() {
+			if v == u {
+				continue;
+			}
+			for t in edges.get(u).unwrap() {
+				if t == u || t == v {
+					continue;
+				}
+				if edges.get(v).unwrap().contains(t) {
+					let mut vec = vec![u, v, t];
+					vec.sort_unstable();
+					result.insert(vec.into_iter().join(","));
+				}
+			}
+		}
+	}
+	result.len()
+}
+
+struct A<'a> {
+	edges: &'a BTreeMap<&'a str, BTreeSet<&'a str>>,
+	r: BTreeSet<&'a str>,
+	p: BTreeSet<&'a str>,
+	x: BTreeSet<&'a str>,
+}
+
+// https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+fn bron<'a>(mut a: A<'a>) -> Vec<BTreeSet<&'a str>> {
+	if a.p.is_empty() && a.x.is_empty() {
+		return vec![a.r];
+	}
+	let p = a.p.clone();
+	let mut q = vec![];
+	for &v in &p {
+		let b = A {
+			edges: a.edges,
+			r: a.r.iter().cloned().chain(std::iter::once(v)).collect(),
+			p: a.p.intersection(a.edges.get(v).unwrap()).cloned().collect(),
+			x: a.x.intersection(a.edges.get(v).unwrap()).cloned().collect(),
+		};
+		q.extend(bron(b));
+		a.p.remove(v);
+		a.x.insert(v);
+	}
+	q
+}
+
+#[aoc(day23, part2)]
+pub fn part2(input: &str) -> String {
+	let (vertices, edges) = parse(input);
+	bron(A{
+		edges: &edges,
+		r: Default::default(),
+		p: vertices,
+		x: Default::default(),
+	}).into_iter().max_by_key(|s| s.len()).unwrap().into_iter().join(",")
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	const INPUT: &str = "
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
+"
+	.trim_ascii();
+
+	#[test]
+	fn test1() {
+		assert_eq!(part1(INPUT), 7);
+	}
+
+	#[test]
+	fn test2() {
+		assert_eq!(part2(INPUT), "co,de,ka,ta");
+	}
+}
--- 2024/src/lib.rs
+++ 2024/src/lib.rs
@@ -31,7 +31,8 @@ pub mod day17;
 pub mod day18;
 pub mod day19;
 pub mod day20;
-pub mod day21;
+//pub mod day21;
 pub mod day22;
+pub mod day23;
 
 aoc_runner_derive::aoc_lib! { year = 2024 }
